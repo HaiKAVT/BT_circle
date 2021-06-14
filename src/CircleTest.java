4fb3e7b5058ec167d96fbc56ffe075064a5112da
HaiKAVT <haivtka@gmail.com>
2021-06-14 11:36:38 +0700

    bai tap thua ke circle

---
+++ src/CircleTest.java
@@ -0,0 +1,11 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+        circle = new Circle(4,"red");
+        System.out.println(circle);
+
+    }
+
+
+}
---
+++ src/CylinderTest.java
@@ -0,0 +1,8 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+        System.out.println(cylinder);
+        cylinder = new Cylinder(3.0,"pink",4.5);
+        System.out.println(cylinder);
+    }
+}
